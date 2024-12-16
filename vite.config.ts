54909b33622713c07f325390bef6b640f39947e5
Richard ten Velde <richard.ten.velde@ivention.nl>
2024-12-16 14:38:30 +0100

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa';
 
 const apiUrl = process.env.API_URL || 'http://localhost:3000'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'android-chrome-192x192.png', 'android-chrome-512x512.png', 'favicon-16x16.png', 'favicon-32x32.png'],
+      registerType: 'autoUpdate', // Automatically update the service worker
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'], // Cache these file types
+      },
+      manifest: {
+        name: 'Recepten',
+        short_name: 'Recepten',
+        description: 'Een app voor het bijhouden van recepten',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
