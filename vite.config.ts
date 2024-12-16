d712858e23cd336cd67ba6354edcdcb09eb180c4
Richard ten Velde <richard.ten.velde@ivention.nl>
2024-12-16 14:51:21 +0100

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: 'web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
