8a1a23f7f7f71d243a78efec5b6f7c28bc45e21c
Richard ten Velde <richard.ten.velde@ivention.nl>
2024-12-29 14:52:08 +0100

    get action

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       ,
       "share_target": {
         "action": "/scraper",
-        "method": "POST",
+        "method": "GET",
         "enctype": "application/x-www-form-urlencoded",
         "params": {
           "text": "url"
