7e2f7596c2f3da345621a111a01e38b991698bc1
Richard ten Velde <richard.ten.velde@ivention.nl>
2025-01-10 22:07:22 +0100

    Fix icons

--- vite.config.ts
+++ vite.config.ts
@@ -22,13 +22,43 @@ export default defineConfig({
       display: 'standalone',
       icons: [
         {
-          src: 'web-app-manifest-192x192.png',
+          src: 'maskable_icon_x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: 'web-app-manifest-512x512.png',
+          src: 'maskable_icon_x512.png',
           sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon_x128.png',
+          sizes: '128x128',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon_x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon_x96.png',
+          sizes: '96x96',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon_x72.png',
+          sizes: '72x72',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon_x48.png',
+          sizes: '48x48',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable_icon.png',
+          sizes: 'any',
           type: 'image/png',
         },
       ]
